22dda81484255f2b13d20bb564fa669ca1a8aa25
Oliver Griffin <olivergriffin2991@gmail.com>
2024-07-21 10:18:04 +0000

    fixed issue with heading padding

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
         //over writes the workspace with new html code
         workspace.innerHTML = `
         <section>
+            <h2 class="hidden-heading">Information Panel</h2>
             <button class="accordion">Ehh... how do I use this?</button>
             <div class="panel">
                 <p>To get started simply choose what unit you're starting with below, input the measurement in the space provided and press the convert button to see your result!</p>
@@ -22,16 +23,16 @@ document.addEventListener("DOMContentLoaded", function () {
                 <h2 class="hidden-heading">The Calculator</h2>
                 <h3>Please Select Type Of Conversion</h3>
                 <div class="buttons">
-                    <button class="options button-29" role="button" data-type="cups-liquid" id="cups-liquid">
+                    <button class="options button-29" data-type="cups-liquid" id="cups-liquid">
                     Cups Liquid
                     </button>
-                    <button class="options button-29" role="button" data-type="cups-solid" id="cups-solid">
+                    <button class="options button-29" data-type="cups-solid" id="cups-solid">
                     Cups Solid
                     </button>
-                    <button class="options button-29" role="button" data-type="ounces" id="ounces">
+                    <button class="options button-29" data-type="ounces" id="ounces">
                     Ounces
                     </button>
-                    <button class="options button-29" role="button" data-type="fluid-ounces" id="fluid-ounces">
+                    <button class="options button-29" data-type="fluid-ounces" id="fluid-ounces">
                     Fluid Ounces
                     </button>
                 </div>
